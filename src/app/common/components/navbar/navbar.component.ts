8d5d7c4a26683e6bf26144e0fc1e57b198aeb70b
homoSanyok <sadha.generalow@yandex.ru>
2025-08-15 11:37:30 +0300

    review navbar

--- src/app/common/components/navbar/navbar.component.ts
+++ src/app/common/components/navbar/navbar.component.ts
@@ -2,12 +2,11 @@
  * @module NavbarComponent
  */
 
-import {Component, inject} from '@angular/core';
+import {Component, computed, inject} from '@angular/core';
 import {SettingsButtonComponent} from '../settings-button/settings-button.component';
 import {SettingsService} from '../../../services/settings.service';
 import {ButtonComponent} from '../../../shared/button/button.component';
 import {LinkT} from '../../types/LinkT';
-import {Tooltip} from "primeng/tooltip";
 
 @Component({
     selector: 'app-navbar',
@@ -15,97 +14,122 @@ import {Tooltip} from "primeng/tooltip";
     imports: [
         SettingsButtonComponent,
         ButtonComponent,
-        Tooltip
     ],
     templateUrl: './navbar.component.html',
     styleUrl: './navbar.component.scss'
 })
 /**
  * Компонент панели навигации.
+ *
+ * Реализует отображение списка элементов link,
+ * списка кнопок редактирования этих элементов, а также
+ * отображает компонент {@link SettingsButtonComponent}.
  */
 export class NavbarComponent {
+    /**
+     * {@link SettingsService}
+     * @private
+     */
     private readonly settings = inject(SettingsService);
 
     /**
-     * Получение списка всех элементов ссылок для шаблона.
+     * Возвращает {@link SettingsService.links} для шаблона.
      */
-    get links() {
+    readonly links = computed(() => {
         return this.settings.links();
-    }
+    });
 
     /**
-     * Получение текущей выбранной ссылки для шаблона.
+     * Возвращает {@link SettingsService.selectedLink} для шаблона.
      */
-    get selectedLink() {
+    readonly selectedLink = computed(() => {
         return this.settings.selectedLink();
-    }
+    });
 
     /**
-     * Получение текущего выбранного для редактирвоания
+     * Возвращает {@link SettingsService.selectedEditingLink} для шаблона.
      */
-    get selectedEditingLink() {
+    readonly selectedEditingLink = computed(() => {
         return this.settings.selectedEditingLink();
-    }
+    });
 
     /**
-     * Возвращает значение сигнала: был ли произведён ресайз области, - для шаблона.
+     * Возвращает булевое значение состояния {@link SettingsService.resize} для шаблона.
+     *
+     * Если размер области был изменён, вернёт `true`.
+     * В противном случае `false`.
      */
-    get isResized() {
+    readonly isResized = computed(() => {
         return Boolean(this.settings.resize());
-    }
+    });
 
     /**
-     * Обработка клика по кнопке ссылки.
-     * Задаёт сигналу `selectedLink` сервиса {@link SettingsService} значение
-     * выбранной ссылки, либо `undefined`, если было произведено нажатие
+     * Обработка нажатия по кнопке элемента link.
+     *
+     * Задаёт сигналу {@link SettingsService.selectedLink} значение
+     * выбранной ссылки из параметра, либо `undefined`, если было произведено нажатие
      * на уже выбранную ссылку.
      *
-     * @param link - выбранный элемент ссылки.
+     * @param link - выбранный элемент link.
      */
     onClickLink(link: LinkT) {
         const selectedLink = this.settings.selectedLink();
-        const selectedEditingLink = this.settings.selectedEditingLink();
 
-        if (selectedEditingLink) {
-            if (selectedLink?.id !== link.id) {
-                this.settings.selectedLink.set(link);
-            }
+        if (this.settings.selectedEditingLink()) {
+            // Если выбран элемент link для редактирования,
+            // выбирает элемент link по которому нажал пользователель,
+            // закрывает панель настроек и сбрасывает выбранный для редактирования элемент.
 
+            this.settings.selectedLink.set(link);
             this.settings.selectedEditingLink.set(undefined);
+            this.settings.open.set(false);
             return;
         }
 
         if (!selectedLink || selectedLink.id !== link.id) {
+            // Если до этого не был выбран элемент link или выбранный ранее
+            // элемент не совпадает с текущим выбранным, выбирает новый элемент.
+
             this.settings.selectedLink.set(link);
             return;
         }
 
+        // Если выбранный элемент совпадает с ранее выбранным,
+        // сбрасывает выбор элемента.
         this.settings.selectedLink.set(undefined);
     }
 
     /**
-     * Обработка клика по кнопке редактирования ссылки.
-     * Задаёт сигналу `selectedEditingLink` сервиса {@link SettingsService} значение
-     * выбранной ссылки, либо `undefined`, если было произведено нажатие
-     * на уже выбранную ссылку.
+     * Обработка нажатия по кнопке редактирования элемента link.
      *
-     * @param link - выбранный элемент ссылки.
+     * Задаёт сигналу {@link SettingsService.selectedEditingLink} значение
+     * выбранного элемента link из параметра, либо `undefined`, если было произведено нажатие
+     * на уже выбранный элемент.
+     *
+     * @param link - выбранный элемент link.
      */
     onClickEditingLink(link: LinkT) {
-        const selectedEditingLink = this.settings.selectedEditingLink();
-        if (selectedEditingLink?.id === link.id) {
-            // При нажатии по уже выбранной кнопке
-            // Убираем выбор меню и кнопки
+        if (this.settings.selectedEditingLink()?.id === link.id) {
+            // Если был выбран ранее выбранный элемент,
+            // затирает текущий выбор меню настроек и элемента для редактирования.
+
             this.settings.selectedEditingLink.set(undefined);
             this.settings.selectedMenu.set(undefined);
             return;
         }
 
-        // Задаём сигналу выбранную кнопку и выбираем меню
+        // Если выбран новый элемент редактирования,
+        // открывает меню настроек edit
         this.settings.selectedEditingLink.set(link);
         this.settings.selectedMenu.set("edit");
     }
 
+    /**
+     * Обработка нажатия на кнопку сброса размера компонента {@link ContentComponent}.
+     *
+     * По нажатии генерирует событие `resetsize`.
+     * В компоненте {@link ContentComponent} прописан слушатель данного события.
+     */
     onClickResizeButton() {
         window.dispatchEvent(new Event('resetsize'));
     }
