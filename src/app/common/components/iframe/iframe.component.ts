d967aa3aa2b70f89f873f377df7f48967d429a93
homoSanyok <sadha.generalow@yandex.ru>
2025-08-15 13:45:43 +0300

    review iframe

--- src/app/common/components/iframe/iframe.component.ts
+++ src/app/common/components/iframe/iframe.component.ts
@@ -1,31 +1,43 @@
-import { Component, computed, ElementRef, inject, viewChild } from '@angular/core';
-import { SettingsService } from '../../../services/settings.service';
-import { DomSanitizer } from '@angular/platform-browser';
-import { v4 as uuid } from 'uuid';
+/**
+ * @module IframeComponent
+ */
+import {Component, computed, inject} from '@angular/core';
+import {SettingsService} from '../../../services/settings.service';
+import {DomSanitizer} from '@angular/platform-browser';
+import {v4 as uuid} from 'uuid';
 
 @Component({
-  selector: 'app-iframe',
-  standalone: true,
-  imports: [],
-  templateUrl: './iframe.component.html',
-  styleUrl: './iframe.component.scss'
+    selector: 'app-iframe',
+    standalone: true,
+    imports: [],
+    templateUrl: './iframe.component.html',
+    styleUrl: './iframe.component.scss'
 })
+/**
+ * Компонент отображающий <iframe> тег, который
+ * открывает выбранный пользователем веб ресурс.
+ */
 export class IframeComponent {
-  private readonly settings = inject(SettingsService);
-  private readonly sanitizer = inject(DomSanitizer); 
+    /**
+     * {@link SettingsService}
+     * @private
+     */
 
-  /**
-   * Мемоизированная ссылка текщего выбранного элемента link для шаблона.
-   */
-  readonly src = computed(() => {
-    let url = this.settings.selectedLink()?.path || '';
-    if (url.indexOf("localhost") !== -1) {
-        // Если обращение идёт на localhost,
-        // значит пользователь захотел рекурсии
-        if (url.at(-1) !== '/') url = `${url}/`;
-        url = `${url}?level=${uuid()}`;
-    }
+    private readonly settings = inject(SettingsService);
+    private readonly sanitizer = inject(DomSanitizer);
 
-    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
-  });
+    /**
+     * Возвращает ссылку текущего выбранного элемента link для шаблона.
+     */
+    readonly src = computed(() => {
+        let url = this.settings.selectedLink()?.path || '';
+        if (url.indexOf("localhost") !== -1) {
+            // Если обращение идёт на localhost,
+            // значит пользователь захотел рекурсии...
+            if (url.at(-1) !== '/') url = `${url}/`;
+            url = `${url}?level=${uuid()}`;
+        }
+
+        return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+    });
 }
