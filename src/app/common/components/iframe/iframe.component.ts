3795b05723e0661221263a68e39e4d4d69a191aa
homoSanyok <sadha.generalow@yandex.ru>
2025-08-20 11:36:28 +0300

    Колесо загрузки сайта

--- src/app/common/components/iframe/iframe.component.ts
+++ src/app/common/components/iframe/iframe.component.ts
@@ -1,15 +1,18 @@
 /**
  * @module IframeComponent
  */
-import {Component, computed, inject} from '@angular/core';
+import {Component, computed, inject, signal, untracked} from '@angular/core';
 import {SettingsService} from '../../../services/settings.service';
 import {DomSanitizer} from '@angular/platform-browser';
 import {v4 as uuid} from 'uuid';
+import {ProgressSpinner} from "primeng/progressspinner";
 
 @Component({
     selector: 'app-iframe',
     standalone: true,
-    imports: [],
+    imports: [
+        ProgressSpinner
+    ],
     templateUrl: './iframe.component.html',
     styleUrl: './iframe.component.scss'
 })
@@ -22,15 +25,26 @@ export class IframeComponent {
      * {@link SettingsService}
      * @private
      */
-
     private readonly settings = inject(SettingsService);
+
     private readonly sanitizer = inject(DomSanitizer);
+
+    /**
+     * Сигнал хранит состояние, просиходит ли сейчас
+     * загрузка сайта.
+     */
+    readonly loading = signal<boolean>(false);
 
     /**
      * Возвращает ссылку текущего выбранного элемента link для шаблона.
      */
     readonly src = computed(() => {
-        let url = this.settings.selectedLink()?.path || '';
+        let url = this.settings.selectedLink()?.path;
+        if (!url) {
+            untracked(() => this.loading.set(false));
+            return;
+        }
+
         if (url.indexOf("localhost") !== -1) {
             // Если обращение идёт на localhost,
             // значит пользователь захотел рекурсии...
@@ -38,6 +52,13 @@ export class IframeComponent {
             url = `${url}?level=${uuid()}`;
         }
 
+        untracked(() => this.loading.set(true));
         return this.sanitizer.bypassSecurityTrustResourceUrl(url);
     });
+
+    onLoaded(event: Event) {
+        const isLoaded = Boolean((event.target as HTMLIFrameElement).src);
+
+        if (isLoaded) this.loading.set(false);
+    }
 }
