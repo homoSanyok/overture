c45433c0345d7054a2e713f68d7653a9adb8b86c
homoSanyok <sadha.generalow@yandex.ru>
2025-11-14 15:32:52 +0300

    anchor-assistant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import {ContentComponent} from './common/components/content/content.component';
 import {ResizeService} from "./services/resize.service";
 import {LinksService} from "./services/links.service";
 
+import {AnchorAssistant, GigaChat, Anchor} from "../../../anchor-assistant/src";
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -16,4 +18,43 @@ import {LinksService} from "./services/links.service";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {}
+export class AppComponent {
+    constructor() {
+        const anchors: Anchor[] = [
+          {
+            selector: "#settings-button",
+            anchor: "кнопка настроек, открыть настройки",
+            parent_selector: "root"
+          }, 
+          {
+            selector: "#link-button",
+            anchor: "открыть ссылку, выбрать элемент",
+            parent_selector: "root"
+          },
+          {
+            selector: "#edit-link-button",
+            anchor: "изменить ссылку, изменить элемент, редактировать ссылку",
+            parent_selector: "#settings-button"
+          },
+          {
+            selector: "#settings-edit", 
+            anchor: "меню создания элемента, добавить ссылку, создать элемент",
+            parent_selector: "#settings-button"
+          },
+          {
+            selector: "#settings-palette", 
+            anchor: "изменить тему, меню смены цветовой схемы приложения",
+            parent_selector: "#settings-button"
+          }
+        ];
+        const connector = new GigaChat(anchors, { 
+          accessor_url: "/accessor",
+          model_url: "/model",
+
+          authorization_key: "OWEzNmJkZmEtOWMwNi00OTg2LTlkOTYtZGMwYzcxNDNhMjZiOmRmNjdhYWRlLWNkNjgtNDJhOS1hNjI3LTg3ZmVkNWQ2NGE4Yw==",
+          scope: "GIGACHAT_API_PERS",
+          model: "GigaChat-2"
+        });
+        new AnchorAssistant(connector, undefined, { delay: 300 });
+    }
+}
